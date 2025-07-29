c0dd17b682f3fba9a8d14baeec744e9d5cac841e
dosaagu <agustind994@gmail.com>
2025-07-29 05:54:34 -0300

    Fix to build

--- src/app/ediciones/[slug]/page.tsx
+++ src/app/ediciones/[slug]/page.tsx
@@ -4,8 +4,19 @@ import matter from 'gray-matter';
 import { remark } from 'remark';
 import html from 'remark-html';
 
-export default async function EdicionPage() {
-  const filePath = path.join(process.cwd(), 'public', 'ediciones', '1.md');
+export async function generateStaticParams() {
+  const edicionesDir = path.join(process.cwd(), 'public', 'ediciones');
+  const files = fs.readdirSync(edicionesDir);
+
+  return files
+    .filter(file => file.endsWith('.md'))
+    .map(file => ({
+      slug: file.replace(/\.md$/, ''),
+    }));
+}
+
+export default async function EdicionPage({ params }: any) {
+  const filePath = path.join(process.cwd(), 'public', 'ediciones', `${params.slug}.md`);
   const fileContents = fs.readFileSync(filePath, 'utf8');
   const { data, content } = matter(fileContents);
   const processedContent = await remark().use(html).process(content);
@@ -15,16 +26,16 @@ export default async function EdicionPage() {
     <div className='bg-gradient-to-b from-base2/50 to-base2/100 p-8 '>
       <div className='flex flex-col justify-center max-w-3xl mx-auto h-screen'>
         <iframe
-          src='/ediciones/pdfs/edicion1.pdf#view=TwoPageView'
+          src={`/ediciones/pdfs/${params.slug}.pdf#view=TwoPageView`}
           className='w-full h-full mt-6'
           title={`PDF de ${data.title}`}
         />
         <p className='text-sm text-gray-500 mb-2'>Publicado el {data.publishedAt}</p>
         {data.pdf && (
           <a
-          href={data.pdf}
-          download
-          className='mt-6 inline-block px-4 py-2 bg-base1 text-white rounded'
+            href={data.pdf}
+            download
+            className='mt-6 inline-block px-4 py-2 bg-base1 text-white rounded'
           >
             Descargar PDF
           </a>
