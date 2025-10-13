ba3fbb26ac99f11704fa8cebe9d8789a43cd15a3
Dosa <agustind994@gmail.com>
2025-10-13 07:34:33 +0000

    Carousel second edition updated

--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -2,7 +2,7 @@ import Carousel from 'react-multi-carousel';
 import 'react-multi-carousel/lib/styles.css';
 import Image from 'next/image';
 import Typewriter from 'typewriter-effect';
-import { sectionsData } from 'app/ediciones/1/data';
+import { sectionsData } from 'app/ediciones/2/data';
 import Link from 'next/link';
 
 interface CarouselComponentProps {
